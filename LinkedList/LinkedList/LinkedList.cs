c55afd8c752746e5d6dc084cdd05092cba32a9e5
merisahakyan <sahakyan_m@bk.ru>
2017-04-10 21:27:19 +0400

    LinkedList

---
+++ LinkedList/LinkedList/LinkedList.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Node<T>
+    {
+        public T value { get; set; }
+        public Node<T> next { get; set; }
+        public Node()
+        {
+
+        }
+        public Node(T data)
+        {
+            this.value = data;
+        }
+    }
+    public class Linked_List<T> : IEnumerable<T>
+    {
+        public Node<T> head;
+        public Node<T> tail;
+        private int count;
+
+
+        public int Count
+        {
+            get { return count; }
+        }
+        public Node<T> First
+        {
+            get
+            {
+                if (head != null)
+                    return head;
+                else return null;
+            }
+        }
+        public Node<T> Last
+        {
+            get
+            {
+                if (tail != null)
+                    return tail;
+                else return null;
+            }
+        }
+
+        public Linked_List()
+        {
+            count = 0;
+        }
+        public Linked_List(IEnumerable<T> collection)
+        {
+            if (collection.Count<T>() != 0)
+            {
+                Node<T> current;
+                collection = collection.Reverse<T>();
+                tail = new Node<T>(collection.First<T>());
+                head = tail;
+                Node<T> next = tail;
+                collection = collection.Skip(1);
+
+                foreach (var m in collection)
+                {
+
+                    current = new Node<T>(m);
+                    current.next = next;
+                    next = current;
+                    head = current;
+
+                }
+            }
+            else
+            {
+                head = null;
+                tail = null;
+            }
+        }
+
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                tail.next = node;
+            }
+            tail = node;
+            count++;
+        }
+
+        public bool Remove(T data)
+        {
+            Node<T> current = head;
+            Node<T> prev = null;
+            while (current != null)
+            {
+                if (current.value.Equals(data))
+                {
+                    if (prev != null)
+                    {
+                        prev.next = current.next;
+                        if (current.next == null)
+                            tail = prev;
+                    }
+                    else
+                    {
+                        head = head.next;
+                        if (head == null)
+                            tail = null;
+                    }
+                    count--;
+                    return true;
+                }
+                prev = current;
+                current = current.next;
+            }
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                yield return current.value;
+                current = current.next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ LinkedList/Project/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinkedList;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Linked_List<int> list = new LinkedList.Linked_List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("after removing {0}", i);
+                list.Remove(i);
+                foreach (var m in list)
+                    Console.WriteLine(m);
+
+                Console.WriteLine("Count={0}", list.Count);
+                Console.WriteLine("First node's value is {0}", list.First.value);
+                Console.WriteLine("Last node's value is {0}", list.Last.value);
+            }
+
+            int[] massive = { 1, 2, 3, 4, 5 };
+            Linked_List<int> mylist = new Linked_List<int>(massive);
+            foreach (var m in mylist)
+                Console.WriteLine(m);
+
+        }
+    }
+}
