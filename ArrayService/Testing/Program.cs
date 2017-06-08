6b2180a4af527a682c908254eb9a27c8a2b31efb
merisahakyan <sahakyan_m@bk.ru>
2017-06-08 12:32:16 +0400

    sorting

--- ArrayService/Testing/Program.cs
+++ ArrayService/Testing/Program.cs
@@ -12,14 +12,17 @@ namespace Testing
         static void Main(string[] args)
         {
             int[] a = Services.CreateArray();
+            //Services.PrintArray(a);
+            //int x = 100, y = 200;
+            //Services.Swap(ref x, ref y);
+            //Console.WriteLine("x=" + x + "; y=" + y);
+            //int[] arr = { 1, 2, 3, 4};
+            // Services.MergeSort(arr, 0, arr.Length);
+            //  Services.MergeSort(a, 0, a.Length - 1);
             Services.PrintArray(a);
-            int x = 100, y = 200;
-            Services.Swap(ref x, ref y);
-            Console.WriteLine("x=" + x + "; y=" + y);
-            int[] arr = { 1, 2, 3, 4};
-           // Services.MergeSort(arr, 0, arr.Length);
-          //  Services.MergeSort(a, 0, a.Length - 1);
-            Services.PrintArray(a);
+            //binary sort doesn't working
+            int[] b = Services.BinarySort(a);
+            Services.PrintArray(b);
         }
     }
 }
